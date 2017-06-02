f9cfae678b70931d1d5695c8cb3f67959370af82
yaosha <174160221@qq.com>
2017-06-02 10:00:22 +0800

    add js unittest

--- test/tests.js
+++ test/tests.js
@@ -1,32 +1,23 @@
 var assert = chai.assert;
 
 describe('newDOMElement(tag, className, id)', function() {
-  describe('#input', function() {
-    it('测试DOM元素是否生成成功', function() {
-      assert.equal('input', newDOMElement('input').tagName);
-      assert.equal('a', newDOMElement('a').tagName);
-      assert.equal('p', newDOMElement('p').tagName);
+  describe('动态生存DOM元素', function() {
+    it('测试生成的DOM元素标签名是否匹配', function() {
+      assert.equal('input', newDOMElement('input').tagName.toLowerCase());
+
+      assert.equal('input', newDOMElement('input','style').tagName.toLowerCase());
+
+      assert.equal('input', newDOMElement('input','style', 'inputid').tagName.toLowerCase());
+    });
+    it('测试生成的DOM元素样式名是否匹配', function() {
+      assert.equal('style', newDOMElement('input','style').className.toLowerCase());
+
+      assert.equal('style', newDOMElement('input','style', 'inputid').className.toLowerCase());
+    });
+    it('测试生成的DOM元素ID是否匹配', function() {
+      assert.equal('inputid', newDOMElement('input','style', 'inputid').id.toLowerCase());
     });
   });
 });
 
-function add() {
-  return Array.prototype.slice.call(arguments).reduce(function(prev, curr) {
-    return prev + curr;
-  }, 0);
-}
 
-describe('add()', function() {
-  var tests = [
-    {args: [1, 2],       expected: 3},
-    {args: [1, 2, 3],    expected: 6},
-    {args: [1, 2, 3, 4], expected: 10}
-  ];
-
-  tests.forEach(function(test) {
-    it('correctly adds ' + test.args.length + ' args', function() {
-      var res = add.apply(null, test.args);
-      assert.equal(res, test.expected);
-    });
-  });
-});
